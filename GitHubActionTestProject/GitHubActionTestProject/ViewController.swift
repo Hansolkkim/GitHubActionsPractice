40dcfef9bb59a772ecf8207c4963a73a892c4116
Sol <hskim6521@naver.com>
2023-12-05 22:04:04 +0900

    feat: add initial project

---
+++ GitHubActionTestProject/GitHubActionTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubActionTestProject
+//
+//  Created by 김한솔 on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
